555510f9075347cf6696ac1b7f49284b8a4d81fd
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2024-12-04 14:57:29 +0000

    class10 consitions and loops

---
+++ 03_Language_Components/09_Loops/c_progress_bar.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Purpose: To display the progress as below:
+ 32% (16 of 50) |###################
+
+ pip install --upgrade progressbar --user
+"""
+import time
+
+import progressbar
+
+bar = progressbar.ProgressBar()
+print(bar)
+
+for i in bar(range(50)):
+    time.sleep(2)  # seconds
